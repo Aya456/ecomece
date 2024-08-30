900c8a46da08f2e639b0f4583465c83c51e3ae20
Aya456 <ayaragab115@gmail.com>
2024-08-30 16:54:25 +0300

    add orders

--- src/components/AllOrders/AllOrders.jsx
+++ src/components/AllOrders/AllOrders.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import style from "./AllOrders.module.css"
+import { Link } from "react-router-dom";
+
 export default function AllOrders() {
-    return (
-        <div>AllOrders</div>
-    )
+  return (
+    <>
+      <div className="bg-[#F8F9FA] w-3/4 mx-auto rounded-md my-6 md:h-[400px]">
+        <div className="flex flex-wrap justify-center items-center py-20">
+          <div className="space-y-3">
+            <h1 className="md:text-[32px] text-[30px]">
+              {" "}
+              Your payment process is successful{" "}
+              <i className="fa-solid fa-circle-check text-[#0891b2]"></i>
+            </h1>
+            <Link to={"/"}>
+              <button
+                type="button"
+                className="block mx-auto btn p-4 w-full my-5 text-sm font-medium text-[#0891b2] focus:outline-none bg-white rounded-lg border border-[#0891b2] hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-[#0891b2] dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                Go Shopping
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
